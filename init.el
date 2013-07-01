4531e019c32dfe46b5e77f193b59869ecc3aa489
aki <spacemanaki@gmail.com>
2013-07-01 12:57:42 -0400

    flip paredit on in clojure-mode

--- init.el
+++ init.el
@@ -100,6 +100,7 @@
 ;; paredit-mode on for lisp
 (autoload 'enable-paredit-mode "paredit" "" t)
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
+(add-hook 'clojure-mode-hook 'enable-paredit-mode)
 
 (add-to-list 'load-path "/Users/aki/source/org-mode/lisp/")
 (add-to-list 'load-path "/Users/aki/source/ob-sml/")
