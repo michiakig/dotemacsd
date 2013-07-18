f4fc8c60705bb8575bc417075d8c0a228c65cb12
aki <spacemanaki@gmail.com>
2013-07-18 17:06:42 -0400

    use IBuffer

--- init.el
+++ init.el
@@ -126,3 +126,6 @@
 (add-to-list 'auto-mode-alist '("\\.vert\\'" . glsl-mode))
 (add-to-list 'auto-mode-alist '("\\.frag\\'" . glsl-mode))
 (add-to-list 'auto-mode-alist '("\\.geom\\'" . glsl-mode))
+
+(defalias 'list-buffers 'ibuffer)
+(setq ibuffer-expert t)
