8bf84d09c420002c4a3bf5553bec1dbfa13e900f
aki <spacemanaki@gmail.com>
2014-05-31 15:13:03 -0400

    tidy package-archives adds

--- init.el
+++ init.el
@@ -1,9 +1,7 @@
 ;; package.el stuff
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/")  t)
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
